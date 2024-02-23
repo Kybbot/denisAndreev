f37476738e7c5fd9de167aa46b61bc60fef44f32
Kybbot <odiykoartem@gmail.com>
2024-02-23 19:46:44 +0200

    Updated Projects section

--- src/js/index.js
+++ src/js/index.js
@@ -113,33 +113,49 @@ for (const mobileMenuLink of mobileMenuLinks) {
 	});
 }
 
-let currentProject = null;
 let currentProjectButton = null;
 const projectsContainer = document.querySelector(".projects__container");
+const projectsModal = document.querySelector("#projectsModal");
+const closeProjectsModal = document.querySelector("#closeProjectsModal");
+
+const projectBtns = document.querySelectorAll(".project__btn");
+
+for (const projectBtn of projectBtns) {
+	projectBtn.addEventListener("click", (event) => {
+		const projectName = event.target.dataset.projectname;
+
+		projectsModal.classList.add("projectsModal--visible");
+		closeProjectsModal.focus();
+		document.body.style.overflow = "hidden";
+	});
+}
+
+closeProjectsModal.addEventListener("click", () => {
+	projectsModal.classList.remove("projectsModal--visible");
+	document.body.style.overflow = "initial";
+	currentProjectButton?.focus();
+});
 
 projectsContainer.addEventListener("mouseover", (event) => {
 	const target = event.target;
 
-	if (target.className === "project__hover") {
+	if (target.className === "project__btn") {
 		const parent = target.parentNode;
-		const btn = parent.querySelector(".project__btn");
+		const btn = parent.querySelector(".project__hover");
 
-		currentProject = parent;
 		currentProjectButton = btn;
 	}
 
 	if (event.target.className === "projects__container") {
-		currentProject = null;
 		currentProjectButton = null;
 	}
 });
 projectsContainer.addEventListener("mouseleave", () => {
-	currentProject = null;
 	currentProjectButton = null;
 });
 
 projectsContainer.addEventListener("mousemove", (event) => {
-	if (currentProjectButton && event.target.className === "project__hover") {
+	if (currentProjectButton && event.target.className === "project__btn") {
 		const layerX = event.layerX - 46;
 		const layerY = event.layerY - 46;
 
