ce1b55dc2687e943289cc72f69d101a4fc96d4ae
Kybbot <odiykoartem@gmail.com>
2024-02-24 16:08:51 +0200

    Updated build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import nunjucks from "vite-plugin-nunjucks";
 
 export default defineConfig({
 	plugins: [nunjucks()],
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: (assetInfo) => {
+					if (assetInfo.name === "index.css") return "assets/index.css";
+					return `assets/${assetInfo.name}`;
+				},
+			},
+		},
+	},
 });
