08f9b61e0efa8ecff1f8dc7a40f7ab24c06e2f4a
Kybbot <odiykoartem@gmail.com>
2024-03-04 22:11:16 +0200

    Added minification for html

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import nunjucks from "vite-plugin-nunjucks";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-	plugins: [nunjucks()],
+	plugins: [nunjucks(), createHtmlPlugin({ minify: true })],
 });
