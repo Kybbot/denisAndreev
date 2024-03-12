2221e510b6e6204446305f084c182153e2a00ae5
Kybbot <odiykoartem@gmail.com>
2024-03-12 13:34:57 +0200

    Updated experience section

--- src/js/experience.js
+++ src/js/experience.js
@@ -3,22 +3,30 @@ export const initExperience = () => {
 	const experienceCards = document.querySelectorAll(".experience__card");
 
 	const updateExperienceCards = () => {
-		for (const experienceCard of experienceCards) {
-			experienceCard.style.height = `initial`;
+		if (window.innerWidth < 768) {
+			for (const experienceCard of experienceCards) {
+				experienceCard.style.height = "initial";
+			}
+
+			experienceCardsWrapper.style.height = "initial";
+		} else {
+			for (const experienceCard of experienceCards) {
+				experienceCard.style.height = `initial`;
+			}
+
+			const arrOfHeights = [];
+			for (const experienceCard of experienceCards) {
+				arrOfHeights.push(experienceCard.getBoundingClientRect().height);
+			}
+
+			const maxHeight = Math.ceil(Math.max(...arrOfHeights));
+
+			for (const experienceCard of experienceCards) {
+				experienceCard.style.height = `${maxHeight + 16}px`;
+			}
+
+			experienceCardsWrapper.style.height = `${maxHeight + maxHeight / 7}px`;
 		}
-
-		const arrOfHeights = [];
-		for (const experienceCard of experienceCards) {
-			arrOfHeights.push(experienceCard.getBoundingClientRect().height);
-		}
-
-		const maxHeight = Math.ceil(Math.max(...arrOfHeights));
-
-		for (const experienceCard of experienceCards) {
-			experienceCard.style.height = `${maxHeight + 16}px`;
-		}
-
-		experienceCardsWrapper.style.height = `${maxHeight + maxHeight / 7}px`;
 	};
 
 	updateExperienceCards();
